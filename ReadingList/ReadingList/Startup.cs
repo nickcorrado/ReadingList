e62ec0f545ccf937f6646e5e8b9b55c1473855ef
Nick Corrado <gcorrado17@jcu.edu>
2017-07-07 17:40:40 -0400

    Created project in Visual Studio.

---
+++ ReadingList/ReadingList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReadingList.Startup))]
+namespace ReadingList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
