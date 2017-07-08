4dc558fdb8fb168d490f0153e8581cd714598cb5
Nick Corrado <gcorrado17@jcu.edu>
2017-07-08 09:19:52 -0400

    Annotated Author model

--- ReadingList/ReadingList/Models/Author.cs
+++ ReadingList/ReadingList/Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,25 @@ namespace ReadingList.Models
     public class Author
     {
         public int AuthorId { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(35, ErrorMessage = "Last name cannot be longer than 35 characters.")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(35, ErrorMessage = "First name cannot be longer than 35 characters.")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
 
         public virtual ICollection<BookAuthor> BookAuthors { get; set; }
     }
