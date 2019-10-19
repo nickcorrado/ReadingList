59029317df67038d74b9d0c244710ac3ba6c33eb
Nick Corrado <gcorrado17@jcu.edu>
2019-10-19 14:19:16 -0400

    Added a proper connection string to a Sql Server Express db.

--- ReadingList/BLL/AuthorService.cs
+++ ReadingList/BLL/AuthorService.cs
@@ -11,47 +11,47 @@ namespace BLL
     //so none of this works anymore as written
     class AuthorService
     {
-        public IList<Author> GetAuthors()
-        {
-            using (var dbContext = new ApplicationDbContext())
-            {
-                var authors = dbContext.Authors.ToList();
-                return authors;
-            }
-        }
+        //public IList<Author> GetAuthors()
+        //{
+        //    using (var dbContext = new ApplicationDbContext())
+        //    {
+        //        var authors = dbContext.Authors.ToList();
+        //        return authors;
+        //    }
+        //}
 
-        public Author GetAuthor(int id)
-        {
-            using (var dbContext = new ApplicationDbContext())
-            {
-                var author = dbContext.Authors.Find(id);
-                return author;
-            }
-        }
+        //public Author GetAuthor(int id)
+        //{
+        //    using (var dbContext = new ApplicationDbContext())
+        //    {
+        //        var author = dbContext.Authors.Find(id);
+        //        return author;
+        //    }
+        //}
 
-        public int Insert(Author author)
-        {
-            using (var dbContext = new ApplicationDbContext())
-            {
-                author.CreateDate = DateTime.Now;
-                dbContext.Authors.Add(author);
-                return dbContext.SaveChanges();
-            }
-        }
+        //public int Insert(Author author)
+        //{
+        //    using (var dbContext = new ApplicationDbContext())
+        //    {
+        //        author.CreateDate = DateTime.Now;
+        //        dbContext.Authors.Add(author);
+        //        return dbContext.SaveChanges();
+        //    }
+        //}
 
-        public int Delete(int id)
-        {
-            using (var dbContext = new ApplicationDbContext())
-            {
-                var author = dbContext.Authors.Find(id);
-                if (author != null)
-                {
-                    dbContext.Authors.Remove(author);
-                    return dbContext.SaveChanges();
-                }
-            }
-            //no such record
-            return 0;
-        }
+        //public int Delete(int id)
+        //{
+        //    using (var dbContext = new ApplicationDbContext())
+        //    {
+        //        var author = dbContext.Authors.Find(id);
+        //        if (author != null)
+        //        {
+        //            dbContext.Authors.Remove(author);
+        //            return dbContext.SaveChanges();
+        //        }
+        //    }
+        //    //no such record
+        //    return 0;
+        //}
     }
 }
