7464373803c72a850e46c958c9b33838b107819b
Nick Corrado <gcorrado17@jcu.edu>
2019-10-19 11:34:27 -0400

    Started adding non-Identity repositories to Data, starting with Author

---
+++ ReadingList/Data/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Configurations
+{
+    internal class AuthorConfiguration : EntityTypeConfiguration<Author>
+    {
+        internal AuthorConfiguration()
+        {
+            ToTable("Authors");
+
+            HasKey(x => x.AuthorId)
+                .Property(x => x.AuthorId)
+                .HasColumnName("AuthorId")
+                .HasColumnType("int")
+                .IsRequired();
+
+            Property(x => x.FirstName)
+                .HasColumnName("FirstName")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(35)
+                .IsRequired();
+
+            Property(x => x.LastName)
+                .HasColumnName("LastName")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(35)
+                .IsRequired();
+
+            Property(x => x.CreateDate)
+                .HasColumnName("CreateDate")
+                .HasColumnType("datetime2")
+                .HasPrecision(7)
+                .IsRequired();
+        }
+    }
+}
---
+++ ReadingList/Data/Repositories/AuthorRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    internal class AuthorRepository : Repository<Author>
+    {
+        internal AuthorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
